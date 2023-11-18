a1f1f63bf76a932ce17956bab381477e373c877c
alessandrolandi <117370617+alessandrolandi@users.noreply.github.com>
2023-11-18 16:11:33 -0500

    Pylint 2 with Black changes

--- web-app/server/app.py
+++ web-app/server/app.py
@@ -1,11 +1,15 @@
 """Module providing routing."""
 from flask import Flask, render_template
 
-app=Flask(__name__, template_folder='../client/templates', static_folder='../client/static')
+app = Flask(
+    __name__, template_folder="../client/templates", static_folder="../client/static"
+)
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
+
 
 if __name__ == "__main__":
     """Function to run app if it's run as a script."""

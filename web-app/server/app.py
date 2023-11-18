7e5313a174db9dd9b06271faee344a9641324ee7
alessandrolandi <117370617+alessandrolandi@users.noreply.github.com>
2023-11-18 15:54:13 -0500

    Lint check 3

--- web-app/server/app.py
+++ web-app/server/app.py
@@ -1,3 +1,4 @@
+"""Module providing routing."""
 from flask import Flask, render_template
 
 app=Flask(__name__, template_folder='../client/templates', static_folder='../client/static')
@@ -7,5 +8,6 @@ def home():
     return render_template('home.html')
 
 if __name__ == "__main__":
+    """Function to run app if it's run as a script."""
     app.run(debug=True)
     

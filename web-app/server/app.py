818a17b497080aa1b64029c8f3e4c7fef7763cbe
Alessandro Landi <117370617+alessandrolandi@users.noreply.github.com>
2023-11-18 15:38:20 -0500

    Update app.py

--- web-app/server/app.py
+++ web-app/server/app.py
@@ -1,12 +1,5 @@
-from pymongo.mongo_client import MongoClient
-import os
 from dotenv import load_dotenv
 from flask import Flask, render_template, request, redirect, abort, url_for, make_response
-import datetime
-from bson.objectid import ObjectId
-import pymongo
-import re
-
 
 app=Flask(__name__, template_folder='../client/templates', static_folder='../client/static')
 

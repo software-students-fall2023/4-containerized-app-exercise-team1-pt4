6872b0c20ff6caba2f96ce13f5edd7e1e0912103
alessandrolandi <117370617+alessandrolandi@users.noreply.github.com>
2023-11-18 15:49:12 -0500

    Lint check 2

--- web-app/server/app.py
+++ web-app/server/app.py
@@ -1,7 +1,4 @@
-from dotenv import load_dotenv
 from flask import Flask, render_template
-
-load_dotenv()
 
 app=Flask(__name__, template_folder='../client/templates', static_folder='../client/static')
 
@@ -11,3 +8,4 @@ def home():
 
 if __name__ == "__main__":
     app.run(debug=True)
+    

43bbc83be832ecfd3ee508f2ee98fbb361282e68
geoffreybudiman91 <geoffrey.budiman@gmail.com>
2023-11-18 15:26:59 -0500

    Fixed issues with linting

--- machine-learning-client/app.py
+++ machine-learning-client/app.py
@@ -1,14 +1,17 @@
+"""ML Client for handling audio transcription using the Deepgram API."""
+
 import json
-from deepgram import Deepgram
 import os
-from dotenv import load_dotenv
 from flask import Flask, jsonify, request, abort, make_response
+import pymongo
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from deepgram import Deepgram
 from flask_cors import CORS
+from dotenv import load_dotenv
 
 load_dotenv()
-uri=os.getenv('URI')
+uri = os.getenv('URI')
 
 deepgram = Deepgram(os.environ.get("DEEPGRAM_API_KEY"))
 
@@ -16,15 +19,16 @@ mongo = MongoClient(uri, server_api=ServerApi('1'))
 
 try:
     mongo.admin.command('ping')
-    print("successfully connected to mongo")
-except Exception as e:
-    print(e)
+    print("Successfully connected to MongoDB.")
+except pymongo.errors.ConnectionFailure as e:
+    print(f"MongoDB connection failed: {e}")
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 @app.route("/api", methods=["POST"])
 async def transcribe():
+    """Transcribe audio file using Deepgram."""
     files = request.files
     form = request.form
     features = form.get("features")
@@ -32,33 +36,25 @@ async def transcribe():
     version = form.get("version")
     tier = form.get("tier")
 
-    dgFeatures = json.loads(features)
-    dgRequest = None
+    dg_features = json.loads(features)
+    dg_request = None
 
-    try:
-        if "file" in files:
-            file = files.get("file")
-            file_content = file.read()
-            dgRequest = {"mimetype": file.mimetype, "buffer": file_content}
+    if "file" in files:
+        file = files.get("file")
+        file_content = file.read()
+        dg_request = {"mimetype": file.mimetype, "buffer": file_content}
 
+    if not dg_request:
+        raise ValueError("No file provided for transcription.")
 
-        if not dgRequest:
-            raise Exception("Error: No file provided for transcription.")
+    transcription = await deepgram.transcription.prerecorded(dg_request, dg_features)
 
-        transcription = await deepgram.transcription.prerecorded(dgRequest, dgFeatures)
-
-        return jsonify(
-            {
-                "model": model,
-                "version": version,
-                "tier": tier,
-                "dgFeatures": dgFeatures,
-                "transcription": transcription,
-            }
-        )
-    except Exception as error:
-        return json_abort(error)
-    
-def json_abort(message):
-    print(message)
-    return abort(make_response(jsonify(err=str(message)), 500))
+    return jsonify(
+        {
+            "model": model,
+            "version": version,
+            "tier": tier,
+            "dg_features": dg_features,
+            "transcription": transcription,
+        }
+    )

d6c5a98057e38da14c744837e5ec0b3db5db9121
ShreyasUjagar <135226247+ShreyasUjagar@users.noreply.github.com>
2023-11-20 02:18:14 -0500

    Update app.py

--- web-app/server/app.py
+++ web-app/server/app.py
@@ -1,16 +1,25 @@
-"""Module providing routing."""
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+import gridfs
 
-app = Flask(
-    __name__, template_folder="../client/templates", static_folder="../client/static"
-)
+app = Flask(__name__, template_folder="../client/templates", static_folder="../client/static")
 
+# Setup MongoDB connection
+mongo_client = MongoClient("mongodb://localhost:27017/")
+db = mongo_client.our_database_name  #  Replace with our database name
+fs = gridfs.GridFS(db)
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
+@app.route("/upload_audio", methods=["POST"])
+def upload_audio():
+    if 'audioFile' in request.files:
+        audio_file = request.files['audioFile']
+        audio_id = fs.put(audio_file)  # Storing the audio file in GridFS
+        return {'status': 'success', 'audio_id': str(audio_id)}, 200
+    return {'status': 'error', 'message': 'No audio file provided'}, 400
 
 if __name__ == "__main__":
-    """Function to run app if it's run as a script."""
     app.run(debug=True)
